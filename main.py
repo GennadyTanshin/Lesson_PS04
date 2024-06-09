63653870af4a011d301d04c65b1c29caabba67f0
GennadyTanshin <163913220+GennadyTanshin@users.noreply.github.com>
2024-06-09 20:54:37 +0300

    update main

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
+
 import time
 
 geckodriver_path = r"C:\Geckodriver\geckodriver.exe"
 service = Service(executable_path=geckodriver_path)
 browser = webdriver.Firefox(service=service)
 browser.get('https://en.wikipedia.org/wiki/Document_Object_Model')
-time.sleep(10)
-browser.quit()
-
+browser.save_screenshot("dom.png")
+time.sleep(5)
+browser.get("https://ru.wikipedia.org/wiki/Selenium")
+browser.save_screenshot("selenium.png")
+time.sleep(3)
+browser.refresh()
 
 
 
