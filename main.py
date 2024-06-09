b132a185b21c3cb9e4fa2ebf9e898e05f22fc62e
GennadyTanshin <163913220+GennadyTanshin@users.noreply.github.com>
2024-06-09 23:32:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,18 +3,27 @@ from selenium.webdriver.firefox.service import Service
 from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
 import time
+import random
 
 geckodriver_path = r"C:\Geckodriver\geckodriver.exe"
 service = Service(executable_path=geckodriver_path)
 browser = webdriver.Firefox(service=service)
-browser.get('https://ru.wikipedia.org/wiki/%D0%97%D0%B0%D0%B3%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F_%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0')
-assert "Википедия" in browser.title
-time.sleep(3)
-search_box=browser.find_element(By.ID, "searchInput")
-search_box.send_keys("Солнечная система")
-search_box.send_keys(Keys.RETURN)
-time.sleep(5)
-a= browser.find_element(By.LINK_TEXT, "Солнечная система")
-a.click()
+browser.get('https://ru.wikipedia.org/wiki/%D0%A1%D0%BE%D0%BB%D0%BD%D0%B5%D1%87%D0%BD%D0%B0%D1%8F_%D1%81%D0%B8%D1%81%D1%82%D0%B5%D0%BC%D0%B0')
+#paragraphs = browser.find_elements(By.TAG_NAME,"p")
+#for paragraph in paragraphs:
+#    print (paragraph.text)
+#    input()
+
+hatnotes = []
+for element in browser.find_elements(By.TAG_NAME, "div"):
+    cl=element.get_attribute("class")
+    if cl == "hatnote navigation-not-searchable":
+        hatnotes.append(element)
+
+print(hatnotes)
+
+hatnote = random.choice(hatnotes)
+link=hatnote.find_element(By.TAG_NAME, "a").get_attribute("href")
+browser.get(link)
 
 
