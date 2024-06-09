95b5fae62432c6abb5a096cf8b4b6e10a1ec9a25
GennadyTanshin <163913220+GennadyTanshin@users.noreply.github.com>
2024-06-09 21:25:43 +0300

    update

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
-
+from selenium.webdriver import Keys
+from selenium.webdriver.common.by import By
 import time
 
 geckodriver_path = r"C:\Geckodriver\geckodriver.exe"
 service = Service(executable_path=geckodriver_path)
 browser = webdriver.Firefox(service=service)
-browser.get('https://en.wikipedia.org/wiki/Document_Object_Model')
-browser.save_screenshot("dom.png")
-time.sleep(5)
-browser.get("https://ru.wikipedia.org/wiki/Selenium")
-browser.save_screenshot("selenium.png")
+browser.get('https://ru.wikipedia.org/wiki/%D0%97%D0%B0%D0%B3%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F_%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0')
+assert "Википедия" in browser.title
 time.sleep(3)
-browser.refresh()
-
+search_box=browser.find_element(By.ID, "searchInput")
+search_box.send_keys("Солнечная система")
+search_box.send_keys(Keys.RETURN)
+time.sleep(5)
+a= browser.find_element(By.LINK_TEXT, "Солнечная система")
+a.click()
 
 
