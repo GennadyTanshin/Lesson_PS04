6e72045fdd0f7aac187f71f78690f088f7738c2e
GennadyTanshin <163913220+GennadyTanshin@users.noreply.github.com>
2024-06-09 16:22:51 +0300

    add servise  for geckodriver

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from selenium import webdriver
+from selenium.webdriver.firefox.service import Service
 import time
 
-
-browser = webdriver.Firefox()
-browser.get("https://en.wikipedia.org/wiki/Document_Object_Model")
+geckodriver_path = r"C:\Geckodriver\geckodriver.exe"
+service = Service(executable_path=geckodriver_path)
+browser = webdriver.Firefox(service=service)
+browser.get('https://en.wikipedia.org/wiki/Document_Object_Model')
 time.sleep(10)
 browser.quit()
+
+
+
 
